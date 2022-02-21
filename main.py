4204e60ba46c8a6fa8ed4305ffb6e1235213d28d
Brianna O <98538246+briannasexton@users.noreply.github.com>
2022-02-21 19:02:20 +0000

    lab

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ michelangelo.color('orange')
 leonardo.color('blue')
 michelangelo.shape('turtle')
 leonardo.shape('turtle')
+michelangelo.speed(speed=1)
+leonardo.speed(speed=1)
 
 michelangelo.up()          # 4.  Pick up the pen so we don’t get lines
 leonardo.up()
@@ -19,8 +21,28 @@ leonardo.goto(-100,-20)
 
 ## 5. your code goes here
 
+michelangelo.forward(random.randrange(1,100))
+leonardo.forward(random.randrange(1,100))
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+
+for i in range(10):
+  michelangelo.forward(random.randrange(0,10))
+  leonardo.forward(random.randrange(0,10))
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
 # Part B - complete part B here
+leonardo.goto(0,0)
+leonardo.down()
+sides= [3,4,6,9,12]
 
+for num_of_sides in sides:
+  for i in range(num_of_sides):
+    leonardo.right(360/num_of_sides)
+    leonardo.forward(40)
+  leonardo.clear()
+leonardo.up()
+leonardo.goto(-100,-20)
 
 window.exitonclick()
